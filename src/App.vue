bb60835544cf052c75bbfd8c1f1b8d3a2d9a9038
Scott <scott@zenbit.ca>
2018-06-20 12:54:43 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    onBackButton (e) {
+      console.log('Exit Event', e)
+    }
+  },
+  mounted () {
+    document.addEventListener('backbutton', this.onBackButton, false)
+  }
+}
+</script>
+
+<style>
+</style>
